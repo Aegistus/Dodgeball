6be6b3175dc62e4e39e0561261a0e11c4452d5a3
Joe Ballanderson <57004318+Aegistus@users.noreply.github.com>
2024-02-10 00:33:40 -0500

    Add Networked Team Colors

--- Assets/Scripts/Team.cs
+++ Assets/Scripts/Team.cs
@@ -5,27 +5,45 @@ using Fusion;
 
 public class Team : NetworkBehaviour
 {
-    [HideInInspector][Networked] public int TeamIndex { get; private set; } = 0;
+    [Networked] public int TeamIndex { get; private set; } = 0;
 
     Renderer[] rends;
 
-    public void SetTeam(int teamIndex)
-    {
-        if (HasStateAuthority)
-        {
-            TeamIndex = teamIndex;
-            rends = GetComponentsInChildren<Renderer>();
-            Material teamMat = TeamManager.GetTeamColor(TeamIndex);
-            if (teamMat != null)
-            {
-                for (int i = 0; i < rends.Length; i++)
-                {
-                    rends[i].sharedMaterial = teamMat;
-                }
-            }
+    ChangeDetector changeDetector;
 
+    public override void Spawned()
+    {
+        changeDetector = GetChangeDetector(ChangeDetector.Source.SimulationState);
+        rends = GetComponentsInChildren<Renderer>();
+        UpdateTeamColor();
+    }
+
+    public override void Render()
+    {
+        foreach (var change in changeDetector.DetectChanges(this))
+        {
+            if (change == nameof(TeamIndex))
+            {
+                UpdateTeamColor();
+            }
         }
     }
 
+    public void SetTeam(int teamIndex)
+    {
+        TeamIndex = teamIndex;
+    }
 
+    public void UpdateTeamColor()
+    {
+        Material teamMat = TeamManager.GetTeamColor(TeamIndex);
+        print(TeamIndex);
+        if (teamMat != null)
+        {
+            for (int i = 0; i < rends.Length; i++)
+            {
+                rends[i].sharedMaterial = teamMat;
+            }
+        }
+    }
 }
