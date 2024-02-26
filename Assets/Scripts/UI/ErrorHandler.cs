b96a5a7e6f0d699bfb3b58e99030b6438593093c
ikdoo1004 <98782167+ikdoo1004@users.noreply.github.com>
2024-02-25 22:02:44 -0500

    Sample UI (ongoing)
    It's from Free Unity Asset 'SmartFoxServer2X' and our own design adjustment is ongoing. 'In-game Scene' which contains in-game chat will be discussed further.

---
+++ Assets/Scripts/UI/ErrorHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ErrorHandler : MonoBehaviour
+{
+    Text errorText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (name.Equals(TheNameOnServer)) {
+            errorText = "That name already exists!";
+        }
+        */
+    }
+
+}
