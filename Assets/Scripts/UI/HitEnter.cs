de4ba7161af326a81e7d632418aafc8e6af335df
ikdoo1004 <98782167+ikdoo1004@users.noreply.github.com>
2024-03-19 21:41:50 -0400

    Non-functional UI Design
    Added Wrench gif as an Option button, exit button, and give it default material for the Login Page background. No buttons are working yet. I'll fix it more when we discuss about it.

--- Assets/Scripts/UI/HitEnter.cs
+++ Assets/Scripts/UI/HitEnter.cs
@@ -13,7 +13,7 @@ public class HitEnter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.KeypadEnter))
         {
             // Scene changes to Lobby
         }
